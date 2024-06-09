18f58898dcf2c440c4e2d40bf3132ebea3c4e14a
AbdallahAwaysheh <awaysheh6930@gmail.com>
2024-06-09 14:55:05 +0300

    a

--- javaScript.js
+++ javaScript.js
@@ -102,7 +102,7 @@
 //         if (sentence[i]===splitBy){
 //             result.push(word);
 //            word="";
-           
+
 //         } else {
 //             word+=sentence[i];
 //         }
@@ -128,3 +128,63 @@
 
 
 // "red,white,blue"
+
+
+
+
+// ------------------------------More Tasks-----------------------------------
+
+// let area ;
+// function calculateArea (radius){
+//     radius*=radius;
+//     radius*=Math.PI;
+//     area=radius;
+//     return area;
+// }
+// console.log(calculateArea(5));
+// //78.53981633974483
+// console.log(calculateArea(10));
+// //314.1592653589793
+
+
+
+
+// let reverseString = function (str) {
+//     let word = "";
+//     for (let i = str.length-1; i >= 0; i--) {
+//         word += str[i];
+//     }
+    
+   
+//     return word;
+    
+// };
+// console.log(reverseString("hello"));
+// console.log(reverseString("abdallah"));
+
+
+
+
+
+// function filterEvenNumbers(numsArray){
+//     let evenNums=[];
+//     for (let i = 0; i < numsArray.length;i++){
+//         if (numsArray[i]%2 == 0 ){
+//             evenNums.push(numsArray[i]);
+//         }
+//     }
+   
+//     return evenNums;
+// }
+// console.log (filterEvenNumbers ([1, 2, 3, 4, 5, 6]));
+
+// console.log (filterEvenNumbers([11, 22, 33, 44, 55]));
+// const library={
+//     books:[],
+//     addBook:function(book){ 
+//             this.books.push(book);
+//          }
+// };
+// library.addBook({title: '1984', author: 'George Orwell'});
+// library.addBook({title: 'To Kill a Mockingbird', author: 'Harper Lee' });
+// console.log(library.books);
